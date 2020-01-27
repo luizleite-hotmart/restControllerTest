64d3d49255bc8630062a6dd895d82aa284435ade
luizleite <xp.luiz@gmail.com>
2020-01-27 05:22:26 -0300

    create service

---
+++ src/main/java/com/luizleiteoliveira/restControllerTest/service/StockServices.java
@@ -0,0 +1,16 @@
+package com.luizleiteoliveira.restControllerTest.service;
+
+import com.luizleiteoliveira.restControllerTest.vo.Stock;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class StockServices {
+
+    public List<Stock> createMockStocks(int howMany) {
+        return IntStream.range(0, howMany).mapToObj(i -> new Stock(new Date(), "STK" + i, BigDecimal.valueOf(i))).collect(Collectors.toList());
+    }
+}
